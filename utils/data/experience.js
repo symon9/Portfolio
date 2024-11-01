4034095d2358e3554c9c9ecae40b18d035e57937
akshitmittal20 <akshitmittal20@gmail.com>
2024-11-01 20:12:57 +0530

    Experince modified

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,18 +3,18 @@ export const experiences = [
     id: 1,
     title: 'Full Stack Developer',
     company: "Cognitensor AI",
-    duration: "(May 2024 - Present)"
+    duration: "(2024 - Present)"
   },
+  // {
+  //   id: 2,
+  //   title: "Full Stack Web Dev.",
+  //   company: "FIVERR (Freelance)",
+  //   duration: "(Jan 2024 - June 2024)"
+  // },
   {
     id: 2,
-    title: "Full Stack Web Dev.",
-    company: "FIVERR (Freelance)",
-    duration: "(Jan 2024 - June 2024)"
-  },
-  {
-    id: 3,
     title: "Software Engineer",
     company: "SOFT FREIGHT LOGIC",
-    duration: "(Dec 2022 - Jan 2024)"
+    duration: "(2022 - 2024)"
   }
 ]
