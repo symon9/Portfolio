3532acf15ede8540b97d24adaf6e0124b2c04ed4
Simon Emmanuel <120514869+symon9@users.noreply.github.com>
2025-02-10 05:37:15 +0100

    Update index.jsx

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -1,11 +1,13 @@
 // @flow strict
-
+"use client";
 import { experiences } from "@/utils/data/experience";
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
+//import GlowCard from "../../helper/glow-card";
 import experience from '/public/lottie/code.json';
+import dynamic from "next/dynamic";
+const GlowCard = dynamic(() => import("../../helper/glow-card"), { ssr: false });
 
 function Experience() {
   return (
