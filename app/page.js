999469d92ce8d0ee09333317bc4e139068f5ae09
Simon Emmanuel <120514869+symon9@users.noreply.github.com>
2025-02-10 06:29:49 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default async function Home() {
     <>
       <HeroSection />
       <AboutSection />
-    {/*<Experience />*/}
+      <Experience />
       <Skills />
       <Projects />
     {/*<Education />*/}
