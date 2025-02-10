336a6241ac70f1d02a5774c99626397266a4a53a
Simon Emmanuel <120514869+symon9@users.noreply.github.com>
2025-02-10 05:53:42 +0100

    comment out to fix bug

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -46,7 +46,7 @@ function Education() {
             <div className="flex flex-col gap-6">
               {
                 educations.map(education => (
-                  <GlowCard key={education.id} identifier={`education-${education.id}`}>
+                  {/*<GlowCard key={education.id} identifier={`education-${education.id}`}>*/}
                     <div className="p-3 relative text-white">
                       <Image
                         src="/blur-23.svg"
@@ -73,7 +73,7 @@ function Education() {
                         </div>
                       </div>
                     </div>
-                  </GlowCard>
+                  {/*</GlowCard>*/}
                 ))
               }
             </div>
